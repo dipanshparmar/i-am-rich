d2348ab83c142c627cc2cbf093a31c4973b765c2
Dipansh Parmar <dipansh@pop-os.localdomain>
2020-12-17 01:12:25 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AppBar"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
